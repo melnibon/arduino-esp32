506f82c357dd7b7e4f61647eefae9f2561c19da7
melnibon <pierre.nerzic@free.fr>
2020-02-23 08:49:32 +0100

    add parameter to configure buffer size
    defaults to 256

--- cores/esp32/HardwareSerial.h
+++ cores/esp32/HardwareSerial.h
@@ -55,7 +55,7 @@ class HardwareSerial: public Stream
 public:
     HardwareSerial(int uart_nr);
 
-    void begin(unsigned long baud, uint32_t config=SERIAL_8N1, int8_t rxPin=-1, int8_t txPin=-1, bool invert=false, unsigned long timeout_ms = 20000UL);
+    void begin(unsigned long baud, uint32_t config=SERIAL_8N1, int8_t rxPin=-1, int8_t txPin=-1, bool invert=false, uint16_t buffersize=256, unsigned long timeout_ms = 20000UL);
     void end();
     void updateBaudRate(unsigned long baud);
     int available(void);
